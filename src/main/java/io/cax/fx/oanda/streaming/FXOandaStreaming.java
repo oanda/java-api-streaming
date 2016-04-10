5502ececefd6a90298d1a539981869d63380e443
Carlos Queiroz <cqueiroz@gopivotal.com>
2016-04-10 13:07:35 +0800

    first commit.

---
+++ src/main/java/io/cax/fx/oanda/streaming/FXOandaStreaming.java
@@ -0,0 +1,63 @@
+package io.cax.fx.oanda.streaming;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+
+@SpringBootApplication
+public class FXOANDAStreaming implements CommandLineRunner{
+
+    @Value("${oanda.access_token}")
+    private String accessToken;
+
+    @Value("${oanda.account_id}")
+    private String accountId;
+
+
+    @Value("${oanda.domain}")
+    private String domain;
+
+    private RestOperations restTemplate;
+
+    public static void main (String[]args) throws IOException {
+        SpringApplication.run(FXOANDAStreaming.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+
+        restTemplate = new RestTemplate();
+
+        String instruments = "EUR_USD,USD_JPY,EUR_JPY";
+
+        UriComponents uriComponents = UriComponentsBuilder.fromUriString(domain).path("/v1/prices").queryParam("accountId",accountId).queryParam("instruments",instruments).build();
+
+        restTemplate.execute(uriComponents.toUriString(),
+                HttpMethod.GET,
+                clientHttpRequest -> clientHttpRequest.getHeaders().add("Authorization","Bearer " + accessToken),
+                clientHttpResponse -> {
+
+                    try(Scanner scanner = new Scanner(clientHttpResponse.getBody(),"utf-8")){
+                        scanner.useDelimiter("\\n");
+                        while(scanner.hasNext()) System.out.println(scanner.next());
+                    }
+
+                    return new ResponseEntity<>(HttpStatus.OK);
+                },
+                Object.class);
+
+    }
+
+}
